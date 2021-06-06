bdf9d9494fbdb6cbdc729589ad2a06ead15ba8e8
YUNIOR JOSE LAUCEL PAEZ <junior.laucel10@gmail.com>
2021-06-06 19:26:52 -0400

    First Upload

---
+++ SistemaVisitas/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(SistemaVisitas.Startup))]
+namespace SistemaVisitas
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
